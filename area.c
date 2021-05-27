57b2ccc95cab54fea469b94aa13e14adbe81364e
MounikaRapuru <84891742+MounikaRapuru@users.noreply.github.com>
2021-05-27 10:05:36 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,13 @@
+/* area & perimeter of circle*/
+#include<stdio.h>
+void main()
+{
+int l,b,area,peri;
+printf("Enter l&b values");
+scanf("%d %d",&l,&b);
+area=l*b;
+peri=2*(l+b);
+printf("Area is %d\n",area);
+printf("Perimeter is %d",peri);
+getch();
+}
